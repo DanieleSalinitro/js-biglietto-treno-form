fa9b978b63ccedc7e736ae27e9c5bc2cfa8c6ad5
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-03-07 23:52:55 +0100

    modifiche ultimate

--- js/script.js
+++ js/script.js
@@ -3,12 +3,6 @@ let elUserKm = document.getElementById('userkm');
 let elRangeAge = document.getElementById('range-age');
 let elButton = document.getElementById('btn-show');
 let elButtonClear = document.getElementById('btn-clear');
-
-let elname = document.querySelector('.ds-namepass');
-let elticket = document.querySelector('.ds-ticket');
-let elcarriage = parseInt(document.querySelector('.ds-carriage'));
-let elCpCode = parseInt(document.querySelector('.ds-cpcode'));
-let elprice = document.querySelector('.ds-price');
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
@@ -23,30 +17,27 @@ elButton.addEventListener('click', function(){
     let price = userKm * priceKm;
     let discount = 0;
     let finalPrice;
-    
+    if (!isNaN(userKm)){
+        if (rangeAge == 'junior') {
+            discount = price * discountJunior;
+            document.querySelector(".ds-ticket").innerHTML = `Biglietto FrecciaYoung` ;
 
-        if (!isNaN(userKm)){
-
-            if (rangeAge == 'junior') {
-                discount = price * discountJunior;
-                document.querySelector(".ds-ticket").innerHTML = `Biglietto FrecciaYoung` ;
-
-            } else if (rangeAge == 'senior') {
-                discount = price * discountSenior;
-                document.querySelector(".ds-ticket").innerHTML = `Biglietto FrecciaSenior` ;
-            } else{
-                discount=0;
-                document.querySelector(".ds-ticket").innerHTML = `Biglietto intero` ;
-            }
-            finalPrice= price - discount;
-            document.querySelector(".ds-train-price").innerHTML = `${finalPrice.toFixed(2)} €`;
-            document.querySelector(".ds-namepass").innerHTML = userName;
-            document.querySelector(".ticket-show").classList.remove('d-none');
-            document.querySelector(".ds-carriage").innerHTML = randomNumber(1,25);
-            document.querySelector(".ds-cpcode").innerHTML = randomNumber(10000,99999);
+        } else if (rangeAge == 'senior') {
+            discount = price * discountSenior;
+            document.querySelector(".ds-ticket").innerHTML = `Biglietto FrecciaSenior` ;
         } else{
-            alert(`ERRORE - Inserire valori numerici`);
+            discount=0;
+            document.querySelector(".ds-ticket").innerHTML = `Biglietto intero` ;
         }
+        finalPrice= price - discount;
+        document.querySelector(".ds-train-price").innerHTML = `${finalPrice.toFixed(2)} €`;
+        document.querySelector(".ds-namepass").innerHTML = userName;
+        document.querySelector(".ticket-show").classList.remove('d-none');
+        document.querySelector(".ds-carriage").innerHTML = randomNumber(1,25);
+        document.querySelector(".ds-cpcode").innerHTML = randomNumber(10000,99999);
+    } else{
+        alert(`ERRORE - Inserire valori numerici`);
+    }
 })
 
 elButtonClear.addEventListener('click', function(){
